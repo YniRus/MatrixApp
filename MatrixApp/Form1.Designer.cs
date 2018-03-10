37bf058a61a57f34f7c5fc93563a986bf1d4d450
YniRus <ynirus@yandex.ru>
2018-03-10 14:54:50 +0300

    Первый функционал
    Возможность ввода порядка матрицы и динамическое изменение её размеров. Заполнение рандомными значениями и обнуление ниже главной диагонали

--- MatrixApp/Form1.Designer.cs
+++ MatrixApp/Form1.Designer.cs
@@ -28,20 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MatrixGrid = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MatrixGrid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(834, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ввведите порядок матрицы";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(834, 108);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(362, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // MatrixGrid
+            // 
+            this.MatrixGrid.AllowUserToAddRows = false;
+            this.MatrixGrid.AllowUserToDeleteRows = false;
+            this.MatrixGrid.AllowUserToResizeColumns = false;
+            this.MatrixGrid.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.MatrixGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.MatrixGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.MatrixGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.MatrixGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MatrixGrid.ColumnHeadersVisible = false;
+            this.MatrixGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MatrixGrid.Location = new System.Drawing.Point(0, 108);
+            this.MatrixGrid.MultiSelect = false;
+            this.MatrixGrid.Name = "MatrixGrid";
+            this.MatrixGrid.RowHeadersVisible = false;
+            this.MatrixGrid.RowHeadersWidth = 22;
+            this.MatrixGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.MatrixGrid.Size = new System.Drawing.Size(834, 400);
+            this.MatrixGrid.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 491);
+            this.ClientSize = new System.Drawing.Size(834, 508);
+            this.Controls.Add(this.MatrixGrid);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MatrixGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView MatrixGrid;
     }
 }
 
--- MatrixApp/Form1.cs
+++ MatrixApp/Form1.cs
@@ -12,9 +12,49 @@ namespace MatrixApp
 {
     public partial class Form1 : Form
     {
+        int N;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void RandomFill()
+        {
+            Random Random = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    MatrixGrid.Rows[i].Cells[j].Value = Random.Next(1, 9);
+                }
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            N =  Convert.ToInt32(textBox1.Text);
+
+            MatrixGrid.Columns.Clear();
+
+            for (int i = 0; i < N; i++)
+            {
+                MatrixGrid.Columns.Add(Convert.ToString(i), Convert.ToString(i));
+                MatrixGrid.Rows.Add();
+            }
+
+            RandomFill();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    MatrixGrid.Rows[i].Cells[j].Value = 0;
+                }
+            }
         }
     }
 }
